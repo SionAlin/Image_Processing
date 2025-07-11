16eac156faf153c51bb42faf2a15345c070b5c1d
Alin <alin.sion878@gmail.com>
2025-07-11 15:11:37 +0300

    Add ImageProcessing.m

---
+++ ImageProcessing.m
@@ -0,0 +1,16 @@
+classdef ImageProcessing
+  
+    properties
+        Property1
+    end
+
+    methods
+        function obj = ImageProcessing(inputArg1,inputArg2)
+           
+        end
+
+        function outputArg = method1(obj,inputArg)
+            
+        end
+    end
+end
